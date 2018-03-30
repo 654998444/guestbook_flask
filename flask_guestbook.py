0541be61faea5bef0592e31a0a9d08247e5086d9
Angus <654998444@qq.com>
2018-03-30 20:15:03 +0800

    multi thread support

--- flask_guestbook.py
+++ flask_guestbook.py
@@ -49,4 +49,4 @@ def tree_hole():
 			length=len(items), items=items[::-1]) 
 
 if __name__ == '__main__':
-		app.run(host='0.0.0.0', port=8080, debug=True)
+		app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
