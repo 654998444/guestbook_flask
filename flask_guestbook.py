ae83d0303b0848cc5b11df7dbadc83279b9fcaf3
Angus <654998444@qq.com>
2018-03-31 20:38:30 +0800

    some annotations

--- flask_guestbook.py
+++ flask_guestbook.py
@@ -16,9 +16,11 @@ bootstrap = Bootstrap(app)
 
 def DBconnect(Guest_object):
 	engine = create_engine('mysql+pymysql://root:123456@localhost:3306\
-			/guestbook?charset=utf8', echo=True)
+			/guestbook?charset=utf8', echo=True)  
+	# engine add sql data with default charset utf8
 
-	# initial table guestbook if not exist
+	# initial table guestbook if not exist 
+	# !! need to drop table guestbook in mysql if not as same as follows
 	metadata = MetaData(bind=engine)
 	guest_table = Table(
 		'guestbook', metadata,
@@ -28,8 +30,10 @@ def DBconnect(Guest_object):
 		Column('time', DateTime, default=datetime.now),
 		mysql_default_charset='utf8')  # very very very important in create a table supporting utf8
 
-	metadata.create_all()
-	clear_mappers()
+	metadata.create_all()  
+	# guest_table = Table('guestbook', metadata, autoload=True)  
+	# for guest_table already exists in metadata
+	clear_mappers()  # clear original mapper in pymysql? or somthing else?
 	mapper(Guest_object, guest_table)  # build mapper
 
 	DBSession = sessionmaker(bind=engine)
