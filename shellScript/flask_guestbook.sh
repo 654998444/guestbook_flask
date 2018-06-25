556d4f3ac12113555c7e3bec3d4099295980181a
anguschowchowxu <654998444@qq.com>
2018-06-25 00:39:02 +0000

    FIX: change flask_guestbook.sh with log file in /var/log

--- shellScript/flask_guestbook.sh
+++ shellScript/flask_guestbook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 /home/ec2-user/guestbook_flask/flask_guestbook.py &> ~/flask_guestbook-$(date +%Y-%m-%d).log 
+python3 /home/ec2-user/guestbook_flask/flask_guestbook.py &> /var/log/flask_guestbook-$(date +%y-%m-%d).log 
