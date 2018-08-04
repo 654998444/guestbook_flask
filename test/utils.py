28aeac14ec68c50df7c69858604e0e4c1e67bd9f
anguschowchowxu <654998444@qq.com>
2018-08-05 02:39:58 +0800

    FIX: jsonify error encountered with colon character

--- test/utils.py
+++ test/utils.py
@@ -3,32 +3,89 @@
 import pandas as pd
 import numpy as np
 import json
+from datetime import datetime
 
-df = pd.read_csv("timeslots.csv",parse_dates=['Day', 'Timestamp UTC ms', \
+categories = ['Personal Life', 'Art & Entertainment', 'Chores', 'Social', \
+        'No category', 'Transport', 'Professional Life', 'Sport & Fitness', \
+        'Health & Hygiene', 'Resting', 'Hobbies']
+colors = ['#FF00FF', '#484891', '#FFDC35', '#5B00AE', '#D0D0D0', '#0080FF', \
+        '#AE0000', '#FFA042', '#8CEA00', '#00FFFF', '#7373B9']
+cateDict = dict(zip(categories, colors))
+
+df = pd.read_csv("templates/timeslots.csv",parse_dates=['Day', 'Timestamp UTC ms', \
 	'Time'])
-df_ = df[df['Day']>df['Day'][0]]
-df_.drop(columns=['Timestamp UTC ms', 'Steps', 'Move'])
-df_['Duration_min'] = df_['Duration ms']/60000
+df['Duration_hrs'] = df['Duration ms'] / (60*60*1000)
+df.drop(columns=['Timestamp UTC ms', 'Steps', 'Move'])
+df['color'] = df['Activity Category'].map(cateDict)
+df['Activity'] = df['Activity'].apply(lambda x: str(x).replace(': ','-'))
 
-def act_split(strings, index=0):
+def actSplit(strings, index=0):
     if pd.isnull(strings):
         return 'NaN'
-    strings = strings.split(': ')
-    if len(strings)==1:
-        if index == 0:
-            return strings[index]
-        else:
-            return 'NaN'
-    return strings[index]
+    strings = strings.split('-')
+    if len(strings)==2:
+        return strings[index]
+    return strings[0]
 
-df_['act1']= df_['Activity'].apply(act_split)
-df_['act2']= df_['Activity'].apply(act_split, index=1)
+def activityData(df, n=10, type=1):
+    '''
+    type = 1
+    return:
+        [{"itemStyle": {"color": df.color, \
+            "name": df[Activity Category], \
+            "children": [
+            {"itemStyle": {"color": df.color}, \
+                "name": df.Activity, \
+                "value": df.Duration_hrs}
+                ]
+        },
+        ...
+        ]
 
-def activity_data(df, n=10):
-	_ = []
-	for i in df[['act1','act2','Duration_min']][:10]:
-		children = dict{'name': i.act2,
-		value: i.Duration_min}
-		_.append(dict{'name': i.act1,
-		'children': children})
-	return _
+    type = 2
+    return: 
+        [{"itemStyle": {"color": df.color, \
+            "name": df.act1, \
+            "children": [
+            {"itemStyle": {"color": df.color}, \
+                "name": df.act2 || df.act1, \
+                "value": df.Duration_hrs}
+                ]
+        },
+        ...
+        ]
+    '''
+    _ = []
+    if type == 2:
+        for i in range(n):
+            children = dict({'name': df.iloc[i,:].act2,
+            'value': df.iloc[i,:].Duration_hrs,
+            'itemStyle': dict(color=df.iloc[i,:].color)})
+            _.append(dict({'name': df.iloc[i,:].act1,
+            'children': [children],
+            'itemStyle': dict(color=df.iloc[i,:].color)}))
+        return _
+    for i in range(n):
+        children = dict({'name': df.iloc[i,:].Activity,
+        'value': df.iloc[i,:].Duration_hrs,
+        'itemStyle': dict(color=df.iloc[i,:].color)})
+        _.append(dict({'name': df.iloc[i,:]['Activity Category'],
+        'children': [children],
+        'itemStyle': dict(color=df.iloc[i,:].color)}))
+    return _
+
+
+def generateData(start='2018-07-29', end='2018-07-31', 
+    file_path='templates/data.json'):
+    global df
+    _ = [int(i) for i in start.split('-')]
+    start = datetime(_[0],_[1],_[2])
+    _ = [int(i) for i in end.split('-')]
+    end = datetime(_[0],_[1],_[2])
+    df_ = df[(df['Time'] > start) & (df['Time'] < end)]
+    df_.loc[:,'act1'] = df_.loc[:,'Activity'].apply(actSplit).values
+    df_.loc[:,'act2']= df_.loc[:,'Activity'].apply(actSplit, index=1).values
+
+    with open(file_path, 'w') as f:
+        print(activityData(df_, len(df_)))
+        json.dump(activityData(df_, len(df_)), f)
