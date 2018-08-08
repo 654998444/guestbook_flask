2f40aad7dace81f4b6121a17d579a54761cc48a0
anguschowchowxu <654998444@qq.com>
2018-08-08 21:59:04 +0800

    TEST: calendar pragh and perfect daily pie graph of eCharts

--- test/utils.py
+++ test/utils.py
@@ -10,14 +10,35 @@ categories = ['Personal Life', 'Art & Entertainment', 'Chores', 'Social', \
         'Health & Hygiene', 'Resting', 'Hobbies']
 colors = ['#FF00FF', '#484891', '#FFDC35', '#5B00AE', '#D0D0D0', '#0080FF', \
         '#AE0000', '#FFA042', '#8CEA00', '#00FFFF', '#7373B9']
+values = np.array([0.5, 0.5, 5, -3, 0, 0, 5, 1, 0, -1, 1.5], dtype=np.float32)
 cateDict = dict(zip(categories, colors))
+valuesDict = dict(zip(categories, values))
 
 df = pd.read_csv("templates/timeslots.csv",parse_dates=['Day', 'Timestamp UTC ms', \
 	'Time'])
+df['Time'] = df['Time'] - pd.Timedelta(hours=8)
+df['Day'] = df['Time'].apply(lambda x: x.strftime('%Y-%m-%d'))
 df['Duration_hrs'] = df['Duration ms'] / (60*60*1000)
 df.drop(columns=['Timestamp UTC ms', 'Steps', 'Move'])
 df['color'] = df['Activity Category'].map(cateDict)
 df['Activity'] = df['Activity'].apply(lambda x: str(x).replace(': ','-'))
+df['point'] = df['Activity Category'].apply(lambda x: valuesDict[x]) * \
+                    df['Duration_hrs']
+
+dailyPoint = pd.Series([0]*365, index=pd.date_range(start='01/01/2018', \
+                    end='31/12/2018', freq='D'), dtype=float)
+tmp = df.groupby(df['Time'].apply(lambda x: x.strftime('%Y-%m-%d')))\
+                ['point'].sum()
+dailyPoint[[pd.to_datetime(x) for x in tmp.index.tolist()]] = tmp.values
+
+
+def generateDailyPoint(df, file_path='templates/dailyPoint.json'):
+    tmp = dict(zip([x.strftime('%Y-%m-%d') for x in df.index], \
+                                                    df.values))
+    with open(file_path, 'w') as f:
+        json.dump(tmp, f)
+
+generateDailyPoint(dailyPoint)
 
 def actSplit(strings, index=0):
     if pd.isnull(strings):
@@ -75,14 +96,14 @@ def activityData(df, n=10, type=1):
     return _
 
 
-def generateData(start='2018-07-29', end='2018-07-31', 
+def generateData(start='2018-07-30', end='2018-07-31', 
     file_path='templates/data.json'):
     global df
     _ = [int(i) for i in start.split('-')]
     start = datetime(_[0],_[1],_[2])
     _ = [int(i) for i in end.split('-')]
     end = datetime(_[0],_[1],_[2])
-    df_ = df[(df['Time'] > start) & (df['Time'] < end)]
+    df_ = df[(df['Time'] >= start) & (df['Time'] < end)]
     df_.loc[:,'act1'] = df_.loc[:,'Activity'].apply(actSplit).values
     df_.loc[:,'act2']= df_.loc[:,'Activity'].apply(actSplit, index=1).values
 
